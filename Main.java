49550efdd1ff8b5ba53fd45cf2ec7b00963b9d67
rcruz148 <ronandocarmo@hotmail.com>
2015-03-23 20:52:05 -0300

    Remocao Comentario - Bug corrigido

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("\n...Calculadora...\n");
 		
-		//Bug corrigido!
-		
 		InterfaceCalculadora ic = new InterfaceCalculadora();
 	}
 	
