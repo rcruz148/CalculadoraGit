6f8701c5aa8363d92b6eea40d77e568c4d778e57
rcruz148 <ronandocarmo@hotmail.com>
2015-03-23 18:51:28 -0300

    Commitando a Instancia da classe InterfaceCalculadora

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("\n...Calculadora...\n");
+		
+		InterfaceCalculadora ic = new InterfaceCalculadora();
+		
 	}
 	
 }
