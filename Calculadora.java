f0557fa2e7fdbaf89bc2085dfff253e8b9caeb30
rcruz148 <ronandocarmo@hotmail.com>
2015-03-23 20:01:56 -0300

    Metodo multiplicar

--- Calculadora.java
+++ Calculadora.java
@@ -11,4 +11,9 @@ public class Calculadora {
 		System.out.println("\nResultado: " + a + " - " + b + " = " + (a-b));
 	}
 
+	//Metodo de Multiplicacao
+	void multiplicar(int a, int b) {
+		System.out.println("\nResultado: " + a + " * " + b + " = " + (a*b));
+	}
+	
 }
