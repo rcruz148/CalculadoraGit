f5d7a933f818aba16e5f6d92b9c6f63943529ca3
rcruz148 <ronandocarmo@hotmail.com>
2015-03-23 19:17:18 -0300

    Adicionando metodo de subtracao

--- Calculadora.java
+++ Calculadora.java
@@ -5,5 +5,10 @@ public class Calculadora {
 	void somar(int a, int b) {
 		System.out.println("\nResultado: " + a + " + " + b + " = " + (a+b));
 	}
+	
+	//Metodo de Subtração
+	void subtrair(int a, int b) {
+		System.out.println("\nResultado: " + a + " - " + b + " = " + (a-b));
+	}
 
 }
